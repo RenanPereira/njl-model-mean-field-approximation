6b6d61241f11dd0c55dc6fabd13ca5d700ef05a9
Renan Pereira <renan.the.physicist@gmail.com>
2026-02-26 15:35:57 +0000

    change to using switch_src_main_and_run.sh where appropriate

--- calculations/su3_3d_cutoff_int_cross_sections/zero_chem_pot/execute_local_main.sh
+++ calculations/su3_3d_cutoff_int_cross_sections/zero_chem_pot/execute_local_main.sh
@@ -1,36 +1,8 @@
 # This script must be executed from the root folder using: 
-# cd calculations/su3_3d_cutoff_int_cross_sections && ./execute_local_main.sh && cd ../../
+# (cd calculations/su3_3d_cutoff_int_cross_sections && ./execute_local_main.sh) 
 
 #!/bin/bash
 
-echo "Script that switches the main.cpp file from the root project by the main.cpp file in this folder before compiling the code."
-echo ""
-
-# Go to project root
-cd .. && cd .. && cd ..
-
-# Clean previous build 
-make clean
-
-# Backup original main.cpp file for later restoration
-cp src/main.cpp src/main_original.cpp && rm src/main.cpp
-
-# Copy local main.cpp to root folder of the project for build step
-cp calculations/su3_3d_cutoff_int_cross_sections/zero_chem_pot/data/main.cpp src/main.cpp
-
-# Compile code with the local main.cpp file
-make -j$(nproc)
-
-# Restore to original main.cpp file
-rm src/main.cpp && cp src/main_original.cpp src/main.cpp && rm src/main_original.cpp
-
-# Copy executable file to data folder where it will be executed
-cp bin/nambuJonaLasinioModel.out calculations/su3_3d_cutoff_int_cross_sections/zero_chem_pot/data
-
-# Run executable
-cd calculations/su3_3d_cutoff_int_cross_sections/zero_chem_pot/data
-./nambuJonaLasinioModel.out
-rm nambuJonaLasinioModel.out
-
-# Go to project root
-cd ..
+main_folder_path="calculations/su3_3d_cutoff_int_cross_sections/zero_chem_pot/data"
+number_of_up_folders_to_makefile="3"
+../../../scripts/utils/switch_src_main_and_run.sh $main_folder_path $number_of_up_folders_to_makefile
