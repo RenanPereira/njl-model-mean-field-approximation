6edf8d2895aabb06f82c221f92db6689c4dfad83
Renan Pereira <renan.the.physicist@gmail.com>
2026-03-02 13:05:57 +0000

    in script that changes src main by local main, provide name of the file as input to script

--- calculations/su3_3d_cutoff_int_cross_sections/zero_chem_pot/execute_local_main.sh
+++ calculations/su3_3d_cutoff_int_cross_sections/zero_chem_pot/execute_local_main.sh
@@ -1,8 +1,10 @@
+#!/bin/bash
+
 # This script must be executed from the root folder using: 
 # (cd calculations/su3_3d_cutoff_int_cross_sections && ./execute_local_main.sh) 
 
-#!/bin/bash
-
 main_folder_path="calculations/su3_3d_cutoff_int_cross_sections/zero_chem_pot/data"
+main_filename="main.cpp"
 number_of_up_folders_to_makefile="3"
-../../../scripts/utils/switch_src_main_and_run.sh $main_folder_path $number_of_up_folders_to_makefile
+
+../../../scripts/utils/switch_src_main_and_run.sh $main_folder_path $main_filename $number_of_up_folders_to_makefile
--- scripts/utils/switch_src_main_and_run.sh
+++ scripts/utils/switch_src_main_and_run.sh
@@ -1,23 +1,27 @@
 #!/bin/bash
 
-# This script automates the process of switching the main file in the src folder by the
-# one present at the path provided by the user, builds the entire C++ code using it,
-# copies the executable to the same path as the one provided by the user.  
-# It takes two arguments: the path to the main file which will be used to substitute the 
-# one in the src directory, and the number of folder to cd up from the directory where the 
-# script is being executed.
+# This script automates the process of:
+# - switching the main file in the src folder by a local one present at the path provided by the user;
+# - building the entire C++ code using it the provided file;
+# - copies the executable to the same path as the one provided by the user;
+# - execute the code.
+#
+# It takes three arguments: the path to the main file which will be used to substitute the 
+# one in the src directory, the name of the file and the number of folder to cd up from 
+# the directory where the script is being executed.
 # 
 # The script creates a backup of the original file main file which is restored after the code is builded
 # with the main file present at the path provided by the user.
 
 # Validate the input arguments
-if [ "$#" -ne 2 ]; then
-    echo "Usage: $0 <main_folder_path> <number_of_up_folders_to_makefile>"
+if [ "$#" -ne 3 ]; then
+    echo "Usage: $0 <main_folder_path> <local_main_filename> <number_of_up_folders_to_makefile>"
     exit 1
 fi
 
 main_folder_path="$1"
-number_of_up_folders_to_makefile="$2"
+local_main_filename="$2"
+number_of_up_folders_to_makefile="$3"
 
 echo "Running the script that switches the main.cpp file from the src folder by the main.cpp file in the folder provided."
 echo ""
@@ -34,7 +38,7 @@ make clean
 cp src/main.cpp src/main.bak && rm src/main.cpp
 
 # Copy local main.cpp to root folder of the project for build step
-cp "${main_folder_path}"/main.cpp src/main.cpp
+cp "${main_folder_path}"/"${local_main_filename}" src/main.cpp
 
 # Compile code with the local main.cpp file
 make -j$(nproc)
@@ -46,7 +50,7 @@ rm src/main.cpp && cp src/main.bak src/main.cpp && rm src/main.bak
 cp bin/nambuJonaLasinioModel.out "${main_folder_path}"
 
 # Run executable
-cd $main_folder_path
+cd "$main_folder_path"
 ./nambuJonaLasinioModel.out
 rm nambuJonaLasinioModel.out
 
