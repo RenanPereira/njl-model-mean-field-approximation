a52e3a88222b120e7219be66e1c154f56fe93ddb
Renan Pereira <renan.the.physicist@gmail.com>
2026-03-04 17:32:34 +0000

    add thermodynamics results for sets B and C (tested to match previous results)

--- calculations/su3_3d_cutoff_thermodynamics/fixed_chem_pot_temp/execute_calculations.sh
+++ calculations/su3_3d_cutoff_thermodynamics/fixed_chem_pot_temp/execute_calculations.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# This script must be executed from the root folder using: 
+# (cd calculations/su3_3d_cutoff_thermodynamics/fixed_chem_pot_temp && ./execute_calculations.sh)
+
 echo "Script that calculates the thermodynamics and in medium quark effective masses for the \
 SU3 NJL model for different parameter sets as functions of temperature at zero checmical potential."
 echo ""
@@ -17,8 +20,13 @@ cp bin/nambuJonaLasinioModel.out $data_folder
 
 cd $data_folder
 
-# SetA
 filename="thermodynamicsSU3NJL3DCutoffFixedChemPotTempSetA.ini"
+./nambuJonaLasinioModel.out use-config-file $filename
+
+filename="thermodynamicsSU3NJL3DCutoffFixedChemPotTempSetB.ini"
+./nambuJonaLasinioModel.out use-config-file $filename
+
+filename="thermodynamicsSU3NJL3DCutoffFixedChemPotTempSetC.ini"
 ./nambuJonaLasinioModel.out use-config-file $filename
 
 rm nambuJonaLasinioModel.out
