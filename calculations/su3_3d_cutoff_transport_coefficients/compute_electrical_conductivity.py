bbfd8de5600422ae1b5e7aca2135af255faac3f3
Renan Pereira <renan.the.physicist@gmail.com>
2026-03-24 21:58:51 +0000

    evaluate and plot the shear viscosity for CEP chemical potential

--- calculations/su3_3d_cutoff_transport_coefficients/compute_electrical_conductivity.py
+++ calculations/su3_3d_cutoff_transport_coefficients/compute_electrical_conductivity.py
@@ -4,24 +4,35 @@ from common_utils.electrical_conductivity import ElectricalConductivity
 path_input_data_folder = "su3_3d_cutoff_quark_relaxation_times/data/"
 path_output_data_folder = "su3_3d_cutoff_transport_coefficients/data/"
 
-
 # set A, zero chemical potential
 parameter_set = "setA"
 
 method = "COMPLETE_COV"
 electrical_conductivity = ElectricalConductivity(
-    path_input_data_folder + f'RelaxationTimes_{parameter_set}_{method}.dat', 
-    path_output_data_folder + f'ElectricalConductivity_{parameter_set}_{method}.dat'
+    path_input_data_folder + f'RelaxationTimes_{parameter_set}_{method}_CP0.dat', 
+    path_output_data_folder + f'ElectricalConductivity_{parameter_set}_{method}_CP0.dat'
+)
+electrical_conductivity = ElectricalConductivity(
+    path_input_data_folder + f'RelaxationTimes_{parameter_set}_{method}_CPCEP.dat', 
+    path_output_data_folder + f'ElectricalConductivity_{parameter_set}_{method}_CPCEP.dat'
 )
 
 method = "KLEVANSKY"
 electrical_conductivity = ElectricalConductivity(
-    path_input_data_folder + f'RelaxationTimes_{parameter_set}_{method}.dat', 
-    path_output_data_folder + f'ElectricalConductivity_{parameter_set}_{method}.dat'
+    path_input_data_folder + f'RelaxationTimes_{parameter_set}_{method}_CP0.dat', 
+    path_output_data_folder + f'ElectricalConductivity_{parameter_set}_{method}_CP0.dat'
+)
+electrical_conductivity = ElectricalConductivity(
+    path_input_data_folder + f'RelaxationTimes_{parameter_set}_{method}_CPCEP.dat', 
+    path_output_data_folder + f'ElectricalConductivity_{parameter_set}_{method}_CPCEP.dat'
 )
 
 method = "ZHUANG"
 electrical_conductivity = ElectricalConductivity(
-    path_input_data_folder + f'RelaxationTimes_{parameter_set}_{method}.dat', 
-    path_output_data_folder + f'ElectricalConductivity_{parameter_set}_{method}.dat'
+    path_input_data_folder + f'RelaxationTimes_{parameter_set}_{method}_CP0.dat', 
+    path_output_data_folder + f'ElectricalConductivity_{parameter_set}_{method}_CP0.dat'
+)
+electrical_conductivity = ElectricalConductivity(
+    path_input_data_folder + f'RelaxationTimes_{parameter_set}_{method}_CPCEP.dat', 
+    path_output_data_folder + f'ElectricalConductivity_{parameter_set}_{method}_CPCEP.dat'
 )
--- calculations/su3_3d_cutoff_transport_coefficients/compute_shear_viscosity.py
+++ calculations/su3_3d_cutoff_transport_coefficients/compute_shear_viscosity.py
@@ -4,24 +4,35 @@ from common_utils.shear_viscosity import ShearViscosity
 path_input_data_folder = "su3_3d_cutoff_quark_relaxation_times/data/"
 path_output_data_folder = "su3_3d_cutoff_transport_coefficients/data/"
 
-
-# set A, zero chemical potential
+# set A
 parameter_set = "setA"
 
 method = "COMPLETE_COV"
 shear_viscosity = ShearViscosity(
-    path_input_data_folder + f'RelaxationTimes_{parameter_set}_{method}.dat', 
-    path_output_data_folder + f'ShearViscosity_{parameter_set}_{method}.dat'
+    path_input_data_folder + f'RelaxationTimes_{parameter_set}_{method}_CP0.dat', 
+    path_output_data_folder + f'ShearViscosity_{parameter_set}_{method}_CP0.dat'
+)
+shear_viscosity = ShearViscosity(
+    path_input_data_folder + f'RelaxationTimes_{parameter_set}_{method}_CPCEP.dat', 
+    path_output_data_folder + f'ShearViscosity_{parameter_set}_{method}_CPCEP.dat'
 )
 
 method = "KLEVANSKY"
 shear_viscosity = ShearViscosity(
-    path_input_data_folder + f'RelaxationTimes_{parameter_set}_{method}.dat', 
-    path_output_data_folder + f'ShearViscosity_{parameter_set}_{method}.dat'
+    path_input_data_folder + f'RelaxationTimes_{parameter_set}_{method}_CP0.dat', 
+    path_output_data_folder + f'ShearViscosity_{parameter_set}_{method}_CP0.dat'
+)
+shear_viscosity = ShearViscosity(
+    path_input_data_folder + f'RelaxationTimes_{parameter_set}_{method}_CPCEP.dat', 
+    path_output_data_folder + f'ShearViscosity_{parameter_set}_{method}_CPCEP.dat'
 )
 
 method = "ZHUANG"
 shear_viscosity = ShearViscosity(
-    path_input_data_folder + f'RelaxationTimes_{parameter_set}_{method}.dat', 
-    path_output_data_folder + f'ShearViscosity_{parameter_set}_{method}.dat'
+    path_input_data_folder + f'RelaxationTimes_{parameter_set}_{method}_CP0.dat', 
+    path_output_data_folder + f'ShearViscosity_{parameter_set}_{method}_CP0.dat'
+)
+shear_viscosity = ShearViscosity(
+    path_input_data_folder + f'RelaxationTimes_{parameter_set}_{method}_CPCEP.dat', 
+    path_output_data_folder + f'ShearViscosity_{parameter_set}_{method}_CPCEP.dat'
 )
