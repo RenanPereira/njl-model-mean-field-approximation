cf418b1866db4151c017070e0018c9a68eb7a2d9
Renan Pereira <renan.the.physicist@gmail.com>
2026-03-25 19:02:12 +0000

    evaluate shear viscosity for CEP chemical potential for method I, II and III and validate for method I with old results

--- calculations/su3_3d_cutoff_transport_coefficients/compute_shear_viscosity.py
+++ calculations/su3_3d_cutoff_transport_coefficients/compute_shear_viscosity.py
@@ -1,26 +1,26 @@
+import os
 from common_utils.shear_viscosity import ShearViscosity
 
 
 path_input_data_folder = "su3_3d_cutoff_quark_relaxation_times/data/"
 path_output_data_folder = "su3_3d_cutoff_transport_coefficients/data/"
 
-# set A
-parameter_set = "setA"
+parameter_sets = ["setA"]
+int_cross_section_methods = ["COMPLETE_COV", "KLEVANSKY", "ZHUANG"]
+suffixes = ["CP0", "CPCEP"]
 
-method = "COMPLETE_COV"
-shear_viscosity = ShearViscosity(
-    path_input_data_folder + f'RelaxationTimes_{parameter_set}_{method}_CP0.dat', 
-    path_output_data_folder + f'ShearViscosity_{parameter_set}_{method}_CP0.dat'
-)
-
-method = "KLEVANSKY"
-shear_viscosity = ShearViscosity(
-    path_input_data_folder + f'RelaxationTimes_{parameter_set}_{method}_CP0.dat', 
-    path_output_data_folder + f'ShearViscosity_{parameter_set}_{method}_CP0.dat'
-)
-
-method = "ZHUANG"
-shear_viscosity = ShearViscosity(
-    path_input_data_folder + f'RelaxationTimes_{parameter_set}_{method}_CP0.dat', 
-    path_output_data_folder + f'ShearViscosity_{parameter_set}_{method}_CP0.dat'
-)
+for parameter_set in parameter_sets:
+    for method in int_cross_section_methods:
+        for suffix in suffixes:
+            
+            input_filepath = path_input_data_folder + f'RelaxationTimes_{parameter_set}_{method}_{suffix}.dat'
+            output_filepath = path_output_data_folder + f'ShearViscosity_{parameter_set}_{method}_{suffix}.dat'
+            
+            if not os.path.exists(input_filepath):
+                print(f"Skipping missing file: {input_filepath}")
+                continue
+            
+            print(f"Calculating the Shear Viscosity based on the file: {input_filepath}")
+            print(f"[{parameter_set} | {method} | {suffix}] Processing...")
+            
+            ShearViscosity(input_filepath, output_filepath)
