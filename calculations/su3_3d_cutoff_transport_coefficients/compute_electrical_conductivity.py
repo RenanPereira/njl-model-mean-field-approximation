80e8d8371fed37335a9dd5cc140a438faa58665c
Renan Pereira <renan.the.physicist@gmail.com>
2026-03-26 16:05:36 +0000

    evaluate electrical conductivity for CEP chemical potential for method I and validate

--- calculations/su3_3d_cutoff_transport_coefficients/compute_electrical_conductivity.py
+++ calculations/su3_3d_cutoff_transport_coefficients/compute_electrical_conductivity.py
@@ -1,26 +1,26 @@
+import os
 from common_utils.electrical_conductivity import ElectricalConductivity
 
 
 path_input_data_folder = "su3_3d_cutoff_quark_relaxation_times/data/"
 path_output_data_folder = "su3_3d_cutoff_transport_coefficients/data/"
 
-# set A, zero chemical potential
-parameter_set = "setA"
+parameter_sets = ["setA"]
+int_cross_section_methods = ["COMPLETE_COV", "KLEVANSKY", "ZHUANG"]
+suffixes = ["CP0", "CPCEP"]
 
-method = "COMPLETE_COV"
-electrical_conductivity = ElectricalConductivity(
-    path_input_data_folder + f'RelaxationTimes_{parameter_set}_{method}_CP0.dat', 
-    path_output_data_folder + f'ElectricalConductivity_{parameter_set}_{method}_CP0.dat'
-)
-
-method = "KLEVANSKY"
-electrical_conductivity = ElectricalConductivity(
-    path_input_data_folder + f'RelaxationTimes_{parameter_set}_{method}_CP0.dat', 
-    path_output_data_folder + f'ElectricalConductivity_{parameter_set}_{method}_CP0.dat'
-)
-
-method = "ZHUANG"
-electrical_conductivity = ElectricalConductivity(
-    path_input_data_folder + f'RelaxationTimes_{parameter_set}_{method}_CP0.dat', 
-    path_output_data_folder + f'ElectricalConductivity_{parameter_set}_{method}_CP0.dat'
-)
+for parameter_set in parameter_sets:
+    for method in int_cross_section_methods:
+        for suffix in suffixes:
+            
+            input_filepath = path_input_data_folder + f'RelaxationTimes_{parameter_set}_{method}_{suffix}.dat'
+            output_filepath = path_output_data_folder + f'ElectricalConductivity_{parameter_set}_{method}_{suffix}.dat'
+            
+            if not os.path.exists(input_filepath):
+                print(f"Skipping missing file: {input_filepath}")
+                continue
+            
+            print(f"Calculating the Electrical Conductivity based on the file: {input_filepath}")
+            print(f"[{parameter_set} | {method} | {suffix}] Processing...")
+            
+            ElectricalConductivity(input_filepath, output_filepath)
