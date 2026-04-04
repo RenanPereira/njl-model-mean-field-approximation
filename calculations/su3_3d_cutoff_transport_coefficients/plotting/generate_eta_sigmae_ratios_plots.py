e2e1380ced3416095801f24728bf32f70f32558e
Renan Pereira <renan.the.physicist@gmail.com>
2026-04-04 17:00:50 +0100

    add eta and sigmae ratio plots passing through the CEP chemical potential; uniformize plots;

--- calculations/su3_3d_cutoff_transport_coefficients/plotting/generate_eta_sigmae_ratios_plots.py
+++ calculations/su3_3d_cutoff_transport_coefficients/plotting/generate_eta_sigmae_ratios_plots.py
@@ -12,7 +12,7 @@ fig_y_size = 6
 path_transport_data_folder = "su3_3d_cutoff_transport_coefficients/data/"
 path_output_plot_folder = "su3_3d_cutoff_transport_coefficients/plots/"
 
-datasets = [
+datasets_cp0 = [
     (
         path_transport_data_folder + "ShearViscosity_setA_COMPLETE_COV_CP0.dat", 
         path_transport_data_folder + "ElectricalConductivity_setA_COMPLETE_COV_CP0.dat", 
@@ -43,7 +43,7 @@ plot_eta_temp_over_sigmae_s_vs_temp(
     fig_dpi,
     fig_x_size,
     fig_y_size,
-    datasets,
+    datasets_cp0,
     "su3_3d_cutoff_thermodynamics/fixed_chem_pot_temp/data/SU3NJL3DCutoffFixedChemPotTemp_setA_TMin0p0_TMax0p5_CPU0p0.dat",
     path_output_plot_folder + "eta_temp_over_sigmae_s_vs_temp_methods_setA_CP0.png",
     "upper right",
@@ -51,21 +51,21 @@ plot_eta_temp_over_sigmae_s_vs_temp(
     ylim=(9.0, 18),
     x_num_ticks=4,
     y_num_ticks=4,
-    x_formatter="%.2f", 
+    x_formatter="%.3f", 
     y_formatter="%.1f",
     annotation_texts=[
         "set A",
-        r"$\mu = 0.0\ \mathrm{GeV}$",
+        r"$\mu [\mathrm{GeV}] = 0.0$",
     ],
     x_annotation=0.05,
-    y_annotation=0.05,
+    y_annotation=0.035,
 )
 
 plot_eta_temp_over_sigmae_s_vs_temp(
     fig_dpi,
     fig_x_size,
     fig_y_size,
-    datasets,
+    datasets_cp0,
     "su3_3d_cutoff_thermodynamics/fixed_chem_pot_temp/data/SU3NJL3DCutoffFixedChemPotTemp_setA_TMin0p0_TMax0p5_CPU0p0.dat",
     path_output_plot_folder + "eta_temp_over_sigmae_s_vs_temp_methods_setA_CP0_zoom.png",
     "upper right",
@@ -87,18 +87,18 @@ plot_eta_over_sigmae_temp2_vs_temp(
     fig_dpi,
     fig_x_size,
     fig_y_size,
-    datasets,
+    datasets_cp0,
     path_output_plot_folder + "eta_over_sigmae_temp2_vs_temp_methods_setA_CP0.png",
     "upper right",
     xlim=(0.120, 0.300),
     ylim=(130, 180),
     x_num_ticks=4,
     y_num_ticks=6,
-    x_formatter="%.2f", 
+    x_formatter="%.3f", 
     y_formatter="%.0f",
     annotation_texts=[
         "set A",
-        r"$\mu = 0.0\ \mathrm{GeV}$",
+        r"$\mu [\mathrm{GeV}] = 0.0$",
     ],
     x_annotation=0.05,
     y_annotation=0.05,
@@ -108,7 +108,7 @@ plot_eta_over_sigmae_temp2_vs_temp(
     fig_dpi,
     fig_x_size,
     fig_y_size,
-    datasets,
+    datasets_cp0,
     path_output_plot_folder + "eta_over_sigmae_temp2_vs_temp_methods_setA_CP0_zoom.png",
     "upper right",
     xlim=(0.200, 0.225),
@@ -119,7 +119,98 @@ plot_eta_over_sigmae_temp2_vs_temp(
     y_formatter="%.0f",
     annotation_texts=[
         "set A",
-        r"$\mu = 0.0\ \mathrm{GeV}$",
+        r"$\mu [\mathrm{GeV}] = 0.0$",
+    ],
+    x_annotation=0.05,
+    y_annotation=0.05,
+)
+
+datasets_cpcep = [
+    (
+        path_transport_data_folder + "ShearViscosity_setA_COMPLETE_COV_CPCEP.dat", 
+        path_transport_data_folder + "ElectricalConductivity_setA_COMPLETE_COV_CPCEP.dat", 
+        r"Method I", 
+        "black", 
+        2, 
+        "-"
+    ),
+    (
+        path_transport_data_folder + "ShearViscosity_setA_KLEVANSKY_CPCEP.dat", 
+        path_transport_data_folder + "ElectricalConductivity_setA_KLEVANSKY_CPCEP.dat", 
+        r"Method II", 
+        "red", 
+        2, 
+        "-"
+    ),
+        (
+        path_transport_data_folder + "ShearViscosity_setA_ZHUANG_CPCEP.dat", 
+        path_transport_data_folder + "ElectricalConductivity_setA_ZHUANG_CPCEP.dat", 
+        r"Method III", 
+        "blue", 
+        2, 
+        "-"
+    ),
+]
+
+plot_eta_temp_over_sigmae_s_vs_temp(
+    fig_dpi,
+    fig_x_size,
+    fig_y_size,
+    datasets_cpcep,
+    "su3_3d_cutoff_thermodynamics/fixed_chem_pot_temp/data/SU3NJL3DCutoffFixedChemPotTemp_SetA_TMin0p0_TMax0p5_CPU0p318434.dat",
+    path_output_plot_folder + "eta_temp_over_sigmae_s_vs_temp_methods_setA_CPCEP.png",
+    "upper right",
+    xlim=(0.040, 0.300),
+    ylim=(4.0, 12),
+    x_num_ticks=5,
+    y_num_ticks=5,
+    x_formatter="%.3f", 
+    y_formatter="%.1f",
+    annotation_texts=[
+        "set A",
+        r"$\mu [\mathrm{GeV}] = \mu_{\mathrm{CEP}}$",
+    ],
+    x_annotation=0.05,
+    y_annotation=0.035,
+)
+
+plot_eta_over_sigmae_temp2_vs_temp(
+    fig_dpi,
+    fig_x_size,
+    fig_y_size,
+    datasets_cpcep,
+    path_output_plot_folder + "eta_over_sigmae_temp2_vs_temp_methods_setA_CPCEP.png",
+    "upper right",
+    xlim=(0.040, 0.300),
+    ylim=(150, 300),
+    x_num_ticks=5,
+    y_num_ticks=4,
+    x_formatter="%.3f", 
+    y_formatter="%.0f",
+    annotation_texts=[
+        "set A",
+        r"$\mu [\mathrm{GeV}] = \mu_{\mathrm{CEP}}$",
+    ],
+    x_annotation=0.05,
+    y_annotation=0.05,
+)
+
+plot_eta_over_sigmae_temp2_vs_temp(
+    fig_dpi,
+    fig_x_size,
+    fig_y_size,
+    datasets_cpcep,
+    path_output_plot_folder + "eta_over_sigmae_temp2_vs_temp_methods_setA_CPCEP_zoom.png",
+    "upper right",
+    xlim=(0.065, 0.070),
+    ylim=(288, 300),
+    x_num_ticks=6,
+    y_num_ticks=6,
+    x_formatter="%.3f", 
+    y_formatter="%.0f",
+    annotation_texts=[
+        "set A",
+        r"$\mu [\mathrm{GeV}] = \mu_{\mathrm{CEP}}$",
     ],
     x_annotation=0.05,
     y_annotation=0.05,
