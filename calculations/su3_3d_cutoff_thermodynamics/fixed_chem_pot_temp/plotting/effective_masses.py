65447053984d6f1b3b809ddd1890ab36f8ca865c
Renan Pereira <renan.the.physicist@gmail.com>
2026-06-15 18:00:31 +0100

    improve thermodynamics plots: effective_masses

--- calculations/su3_3d_cutoff_thermodynamics/fixed_chem_pot_temp/plotting/effective_masses.py
+++ calculations/su3_3d_cutoff_thermodynamics/fixed_chem_pot_temp/plotting/effective_masses.py
@@ -25,7 +25,7 @@ def plot_quark_masses_vs_temperature(
     legend_loc: str | None = None,
     legend_fontsize: int = 18,
     labels_fontsize: int = 22,
-    label_quark_density: str | None = None,
+    label_effective_quark_mass: str | None = None,
     xlim: tuple[float, float] = (0.0 , 1.0),
     ylim: tuple[float, float] = (0.0 , 1.0),
     x_num_ticks: int = 6,
@@ -56,12 +56,10 @@ def plot_quark_masses_vs_temperature(
 
     # Create a new figure
     fig, ax = plt.subplots(figsize=(fig_x_size, fig_y_size), dpi=fig_dpi)
-
-    temperature = data.get_temperature()
     
     for data, quark_species, label, color, linewidth, linestyle in datasets:
         ax.plot(
-            temperature,
+            data.get_temperature(),
             data.get_quark_effective_mass(quark_species), 
             label=label, 
             color=color, 
@@ -78,10 +76,10 @@ def plot_quark_masses_vs_temperature(
 
     # Axes labels
     ax.set_xlabel(r'$T\, [\mathrm{GeV}]$', fontsize=labels_fontsize)
-    if label_quark_density is None:
+    if label_effective_quark_mass is None:
         ax.set_ylabel(r'$M_q\, [\mathrm{GeV}]$', fontsize=labels_fontsize)
     else: 
-        ax.set_ylabel(label_quark_density, fontsize=labels_fontsize)
+        ax.set_ylabel(label_effective_quark_mass, fontsize=labels_fontsize)
 
     xmin = xlim[0]
     xmax = xlim[1]
@@ -133,7 +131,7 @@ def plot_normalized_quark_masses_vs_temperature(
     legend_loc: str | None = None,
     legend_fontsize: int = 18,
     labels_fontsize: int = 22,
-    label_quark_density: str | None = None,
+    label_normalized_effective_quark_mass: str | None = None,
     xlim: tuple[float, float] = (0.0 , 1.0),
     ylim: tuple[float, float] = (0.0 , 1.0),
     x_num_ticks: int = 6,
@@ -165,14 +163,12 @@ def plot_normalized_quark_masses_vs_temperature(
 
     # Create a new figure
     fig, ax = plt.subplots(figsize=(fig_x_size, fig_y_size), dpi=fig_dpi)
-
-    temperature = data.get_temperature()
     
     for data, quark_species, label, color, linewidth, linestyle in datasets:
         quark_mass_vac = data.get_quark_effective_mass(quark_species)[0]
         
         ax.plot(
-            temperature,
+            data.get_temperature(),
             data.get_quark_effective_mass(quark_species)/quark_mass_vac, 
             label=label, 
             color=color, 
@@ -189,10 +185,10 @@ def plot_normalized_quark_masses_vs_temperature(
 
     # Axes labels
     ax.set_xlabel(r'$T\, [\mathrm{GeV}]$', fontsize=labels_fontsize)
-    if label_quark_density is None:
+    if label_normalized_effective_quark_mass is None:
         ax.set_ylabel(r'$M_\ell/M_\ell^{\mathrm{vac}}$', fontsize=labels_fontsize)
     else: 
-        ax.set_ylabel(label_quark_density, fontsize=labels_fontsize)
+        ax.set_ylabel(label_normalized_effective_quark_mass, fontsize=labels_fontsize)
 
     xmin = xlim[0]
     xmax = xlim[1]
