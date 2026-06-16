420ad1d30c55fe7ab3f867eca038b5f171c6ef45
Renan Pereira <renan.the.physicist@gmail.com>
2026-06-16 13:15:01 +0100

    improve thermodynamics plots: pressure

--- calculations/su3_3d_cutoff_thermodynamics/fixed_chem_pot_temp/build_plots.sh
+++ calculations/su3_3d_cutoff_thermodynamics/fixed_chem_pot_temp/build_plots.sh
@@ -7,7 +7,7 @@ cd ../../
 
 python3 -m su3_3d_cutoff_thermodynamics.fixed_chem_pot_temp.plotting.generate_effective_masses
 python3 -m su3_3d_cutoff_thermodynamics.fixed_chem_pot_temp.plotting.generate_entropy_density
-python3 -m su3_3d_cutoff_thermodynamics.fixed_chem_pot_temp.plotting.pressure
+python3 -m su3_3d_cutoff_thermodynamics.fixed_chem_pot_temp.plotting.generate_pressure
 python3 -m su3_3d_cutoff_thermodynamics.fixed_chem_pot_temp.plotting.generate_energy_density_plots
 python3 -m su3_3d_cutoff_thermodynamics.fixed_chem_pot_temp.plotting.pressure_energy_density
 python3 -m su3_3d_cutoff_thermodynamics.fixed_chem_pot_temp.plotting.generate_quark_density_plots
