9afcf3d0b5512a03eac593cd7d8e49d5a81c9215
Renan Pereira <renan.the.physicist@gmail.com>
2026-06-17 22:38:06 +0100

    add cross section lite calculation to pipeline

---
+++ calculations/su3_3d_cutoff_cross_sections_lite/execute_calculations.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "Script that calculates the Cross Sections for the NJL model."
+echo "This calculation is for testing purposes only: a small sample of points are calculated."
+echo ""
+
+cd ../.. || exit
+
+make -j"$threads"
+
+cp bin/nambuJonaLasinioModel.out calculations/su3_3d_cutoff_cross_sections_lite/data
+
+cd calculations/su3_3d_cutoff_cross_sections_lite/data
+
+./nambuJonaLasinioModel.out use-config-file crossSections_T0p215000_CP0p000000.ini
+
+rm nambuJonaLasinioModel.out
+
+cd ..
