e2cfaa1b38cd3f0dde96925eb995ba058683e0ab
Renan Pereira <renan.the.physicist@gmail.com>
2026-06-18 12:38:35 +0100

    add cross section lite calculation to pipeline

--- calculations/su3_3d_cutoff_cross_sections_test/execute_calculations.sh
+++ calculations/su3_3d_cutoff_cross_sections_test/execute_calculations.sh
@@ -6,14 +6,36 @@ echo ""
 
 cd ../.. || exit
 
+# Set threads: use argument if provided, otherwise use all cores
+echo "This calculation can be parallelized."
+machine_threads=$(nproc)
+echo "This machine has a total of $machine_threads threads."
+
+if [ -n "$1" ]; then
+    if [[ "$1" =~ ^[0-9]+$ ]] && [ "$1" -le "$machine_threads" ]; then
+        threads=$1
+    else
+        echo "Error: Please provide a valid number of threads ≤ $machine_threads."
+        exit 1
+    fi
+else
+    threads=$((machine_threads - 1))
+fi
+echo "The calculation will be executed using $threads threads."
+echo ""
+
 make -j"$threads"
 
 cp bin/nambuJonaLasinioModel.out calculations/su3_3d_cutoff_cross_sections_test/data
 
 cd calculations/su3_3d_cutoff_cross_sections_test/data
 
+sed -i "s/^numberOfThreads = .*/numberOfThreads = $threads/" crossSections_T0p215000_CP0p000000.ini
+
 ./nambuJonaLasinioModel.out use-config-file crossSections_T0p215000_CP0p000000.ini
 
 rm nambuJonaLasinioModel.out
+
+sed -i "s/^numberOfThreads = .*/numberOfThreads = 2/" crossSections_T0p215000_CP0p000000.ini
 
 cd ..
