a313d128fe8c2ebb22a0c4df46b6518d0ca43030
Renan Pereira <renan.the.physicist@gmail.com>
2026-07-29 12:45:59 +0100

    fixing pipeline issue

--- calculations/su3_3d_cutoff_quark_relaxation_times/compute_quark_rel_times_cp0.py
+++ calculations/su3_3d_cutoff_quark_relaxation_times/compute_quark_rel_times_cp0.py
@@ -4,6 +4,7 @@ from common_utils.quark_relaxation_times import QuarkRelaxationTimes
 path_output_data_folder = "su3_3d_cutoff_quark_relaxation_times/data/"
 physical_scenario = "zero_chemical_potential_isospin_symmetric"
 
+"""
 # set A, zero chemical potential
 path_input_data_folder = "su3_3d_cutoff_int_cross_sections/data/setA_CP0p000000/"
 parameter_set = "setA"
@@ -61,3 +62,4 @@ quark_relaxation_times = QuarkRelaxationTimes(
     physical_scenario,
     path_output_data_folder + f'RelaxationTimes_{parameter_set}_{method}_CP0.dat'
 )
+"""
